3c42e7e15130085de049febc6760781de2e937a5
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-04-18 16:41:22 +0200

    Autentication -Admin

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="controller py-5 ">
+
+    <h1>Pagina di Amministrazione</h1>
+
+</div>
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +31,8 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+//Rotta pagina Admin
+
+Route::get('/admin', [DashboardController::class, 'index'])->middleware(['auth']);
